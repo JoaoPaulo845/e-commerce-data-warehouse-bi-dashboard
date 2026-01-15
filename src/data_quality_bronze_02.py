af9a1b472c188e02e7d3558ecfa3c8e9b26c64a8
JoaoPaulo845 <jp200298@gmail.com>
2026-01-14 21:40:57 -0300

    Atualização corretiva

--- src/data_quality_bronze_02.py
+++ src/data_quality_bronze_02.py
@@ -1,78 +1,84 @@
 import duckdb
 from config_path_00 import DB_CAMINHO
 
+# ==========================================
+# ======== Data Quality (Bronze) ===========
+# ==========================================
+
+# Objetivo: Identificar as inconsistências para tratar na camada silver
+# Aqui irei identificar campos Nulos, Duplicados e Valores negativos inconsistêntes
+
 def main():
     con = duckdb.connect(str(DB_CAMINHO))
-    con.execute("CREATE SCHEMA IF NOT EXISTS silver_schema;")
 
-    # Orders - datas e atraso em dias
-    con.execute("""
-        CREATE OR REPLACE TABLE silver_schema.orders AS
+    # Tamanho das tabelas - contagem de linhas
+    print("\nContagem de Linhas:")
+    print(con.execute("""
+        (SELECT 'orders' AS tabela, COUNT(*) AS linhas FROM bronze_schema.orders)
+        UNION ALL
+        (SELECT 'order_items', COUNT(*) FROM bronze_schema.order_items)
+        UNION ALL
+        (SELECT 'customers', COUNT(*) FROM bronze_schema.customers)
+        UNION ALL
+        (SELECT 'products', COUNT(*) FROM bronze_schema.products)
+        UNION ALL
+        (SELECT 'category_translate', COUNT(*) FROM bronze_schema.category_translate)
+    """).fetchdf())  # fetchdf() retorna um dataframe, o que torna mais fácil a visualização
+
+    # A partir daqui selecionarei as colunas com mais impacto no negócio e que podem ser usadas em joins
+
+    # Campos nulos
+    print("\nContagem de nulos:")
+
+    print("\nOrders:")
+    print(con.execute("""
         SELECT
-            order_id,
-            customer_id,
-            order_status,
-            CAST(order_purchase_timestamp AS TIMESTAMP) AS purchase_ts,
-            CAST(order_delivered_customer_date AS TIMESTAMP) AS delivered_customer_ts,
-            CAST(order_estimated_delivery_date AS TIMESTAMP) AS estimated_delivery_ts,
-            CASE
-                WHEN order_delivered_customer_date IS NULL OR order_estimated_delivery_date IS NULL THEN NULL
-                ELSE DATE_DIFF(
-                    'day',
-                    CAST(order_estimated_delivery_date AS DATE),
-                    CAST(order_delivered_customer_date AS DATE)
-                )
-            END AS atraso_dias
+            SUM(CASE WHEN order_id IS NULL THEN 1 ELSE 0 END) AS order_id_nulos,
+            SUM(CASE WHEN customer_id IS NULL THEN 1 ELSE 0 END) AS customer_id_nulos
         FROM bronze_schema.orders;
-    """)
+    """).fetchdf())
 
-    # Order items - criação da coluna receita - Preço + Frete
-    con.execute("""
-        CREATE OR REPLACE TABLE silver_schema.order_items AS
+    print("\nOrder_items:")
+    print(con.execute("""
+        SELECT 
+            SUM(CASE WHEN order_id IS NULL THEN 1 ELSE 0 END) AS order_id_nulos,
+            SUM(CASE WHEN product_id IS NULL THEN 1 ELSE 0 END) AS product_id_nulos,
+            SUM(CASE WHEN order_item_id IS NULL THEN 1 ELSE 0 END) AS order_item_id_nulos
+                FROM bronze_schema.order_items;
+    """).fetchdf())
+
+    # Duplicados
+    print("\nChecagem de duplicados:")
+
+    print("\nOrders (duplicados por order_id):")
+    print(con.execute("""
         SELECT
-            order_id,
-            order_item_id,
-            product_id,
-            price AS receita_item,
-            freight_value AS frete_item,
-            (price + freight_value) AS receita_total_item
+            COUNT(*) AS linhas,
+            COUNT(DISTINCT order_id) AS order_ids_distintos,
+            (COUNT(*) - COUNT(DISTINCT order_id)) AS duplicados
+        FROM bronze_schema.orders;
+    """).fetchdf())
+
+    print("\nOrder_items & order_id - Duas chaves:")
+    print(con.execute("""
+        SELECT
+            COUNT(*) AS linhas,
+            COUNT(DISTINCT nCamada Silver criada com sucesso || '-' || order_item_id) AS chaves_distintas,
+            (COUNT(*) - COUNT(DISTINCT order_id || '-' || order_item_id)) AS duplicados
         FROM bronze_schema.order_items;
-    """)
+    """).fetchdf())
 
-    # Customers 
-    con.execute("""
-        CREATE OR REPLACE TABLE silver_schema.customers AS
+    # Valores negativos - não faz sentido para preço ou frete
+    print("\nChecagem de valores negativos:")
+    print(con.execute("""
         SELECT
-            customer_id,
-            customer_unique_id,
-            customer_city,
-            customer_state
-        FROM bronze_schema.customers;
-    """)
-
-    # Products com tradução
-    con.execute("""
-        CREATE OR REPLACE TABLE silver_schema.products AS
-        SELECT
-            p.product_id,
-            p.product_category_name,
-            CASE
-                WHEN t.product_category_name_english IS NULL THEN p.product_category_name
-                ELSE t.product_category_name_english
-            END AS product_category_name_english
-        FROM bronze_schema.products p
-        LEFT JOIN bronze_schema.category_translate t
-            ON p.product_category_name = t.product_category_name;
-    """)
-
-    print("\nSelect para checagem:")
-    print(con.execute("SELECT * FROM silver_schema.orders LIMIT 3").fetchdf())
-    print(con.execute("SELECT * FROM silver_schema.order_items LIMIT 3").fetchdf())
-    print(con.execute("SELECT * FROM silver_schema.customers LIMIT 3").fetchdf())
-    print(con.execute("SELECT * FROM silver_schema.products LIMIT 3").fetchdf())
+            SUM(CASE WHEN price < 0 THEN 1 ELSE 0 END) AS price_negativo,
+            SUM(CASE WHEN freight_value < 0 THEN 1 ELSE 0 END) AS frete_negativo
+        FROM bronze_schema.order_items;
+    """).fetchdf())
 
     con.close()
-    print("\nCamada Bronze concluída com sucesso!")
+    print("\nCamada Bronze criada com sucesso!")
 
 if __name__ == "__main__":
     main()
